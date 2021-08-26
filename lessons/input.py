cc4eb5e40afb680c812224586b65d973d9dc9403
Sarah Zhang <sz1204@ad.unc.edu>
2021-08-26 11:46:21 -0400

    Testing user input in Python

---
+++ lessons/input.py
@@ -0,0 +1,9 @@
+"""Examples of user input and variables."""
+# Docstring. Serves as user documentation only. Best practice to use.
+
+print("Wow, " + input("Who are you? ") + ", you rock!")
+# String concatenation
+print(input("Who are you? ") + " , have a great day!")
+
+# If you see the white circle on top, save first: CTRL + S 
+# In order to run the program in the terminal, run a module: python -m lessons.input
