f11f0978ec0ff29454daec1f431e0b80959890a5
Sarah Zhang <sz1204@ad.unc.edu>
2021-08-27 12:15:17 -0400

    Completed COMP 110 exercise 01.

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,7 @@
-# TODO: Write docstring here
+"""This program asks the user to input their name and then gives them some positive vibes!"""
+__author__ = "730490960"
 
-# TODO: Initialize __author__ variable here
-
-# TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print(name + ", you're doing a great job!")
+print("Keep pushing forwards, " + name + "!")
+print("Guess what, " + name + ", you're doing fantastic!")
---
+++ exercises/ex01/relational_operators.py
@@ -0,0 +1,13 @@
+"""This program asks the user to input two numbers and then compares them using relational operators."""
+__author__ = "730490960"
+
+left: int = int(input("Please enter a number for the left-hand side: "))
+right: int = int(input("Please enter a number for the right-hand side: "))
+
+print("Left-hand side: " + str(left))
+print("Right-hand side: " + str(right))
+
+print(str(left) + " < " + str(right) + " is " + (str(bool(left<right))))
+print(str(left) + " >= " + str(right) + " is " + str(bool(left>=right)))
+print(str(left) + " == " + str(right) + " is " + str(bool(left==right)))
+print(str(left) + " != " + str(right) + " is " + str(bool(left!=right)))
