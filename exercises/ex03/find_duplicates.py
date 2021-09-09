b36ed6f582e748211ff2ba17d398e855d7ccf0bf
Sarah Zhang <sz1204@ad.unc.edu>
2021-09-09 18:09:22 -0400

    Progress on Exercise 3!

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,21 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730490960"
+
+word: str = input("Enter a word: ")
+i: int = 0
+dupe: bool = False
+
+while i < len(word):
+    j: int = 0
+    while j < len(word):
+        if i == j:
+            j = j + 1
+        else:
+            if word[i] == word[j]:
+                dupe = True
+            j = j + 1
+
+    i = i + 1
+
+print("Found duplicate: " + str(dupe))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,20 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730490960"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+ask: int = int(input("Depth: "))
+i: int = 1
+k: int = 1
+add: str = ""
+
+while i <= ask:
+
+    while i >= k:
+        add = add + TREE
+        k = k + 1
+
+    i = i + 1
+    print(add)
