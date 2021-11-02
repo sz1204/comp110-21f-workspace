37f2d3e56be7f686d195601b40cbce834f330654
Sarah Zhang <sz1204@ad.unc.edu>
2021-11-02 16:11:00 -0400

    Completion of OOP lesson.

--- lessons/classes_objects.py
+++ lessons/classes_objects.py
@@ -4,21 +4,46 @@
 class Pizza:
     """Models the idea of a Pizza."""
 
-    # Attribute Definitions
+    # Attribute Definitions. It is possible to create default values.
     size: str
-    toppings: int 
-    extra_cheese: bool
+    toppings: int
+    extra_cheese: bool = False
+
+    def __init__(self, size: str, toppings: int):
+        """Constructor definition for initialization of attributes."""
+        # Do not define a return type.
+        # This is a special method and is automatically called when a new Pizza object is called.
+        self.size = size
+        self.toppings = toppings
+
+    def price(self, tax: float) -> float:
+        """Calculate the price of a Pizza."""
+        total: float = 0.0
+        if self.size == "large":
+            total += 10.0
+        else:
+            total += 8.0
+        
+        total += self.toppings * 0.75
+
+        if self.extra_cheese:
+            total += 1.0
+
+        total *= tax
+
+        return total
 
 
-a_pizza: Pizza = Pizza()
-a_pizza.size = "large"
-a_pizza.toppings = 3
-a_pizza.extra_cheese = False
+a_pizza: Pizza = Pizza("large", 3)
+# ^ This is a constructor call.
 print(Pizza)
 print(a_pizza)
 print(a_pizza.size)
+print(f"Price: ${a_pizza.price(1.05)}")
+# Giving a parameter: 5% tax. 
 
-b_pizza: Pizza = Pizza()
-b_pizza.size = "small"
-b_pizza.toppings = 0
-b_pizza.extra_cheese = True
+another_pizza: Pizza = Pizza("small", 0)
+another_pizza.extra_cheese = True
+print(another_pizza.size)
+print(f"Price: ${another_pizza.price(1.05)}")
+# Possible to have multiple Pizza objects
